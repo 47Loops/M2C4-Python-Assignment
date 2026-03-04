f1e2a7a6c0506497eea1459232fa5a4380aff34f
Carlos Fernandez Miguel <carlosfernandezmiguel@gmail.com>
2026-03-04 20:32:00 +0000

    Último commit del día.

--- M2C4-python-assignment.py
+++ M2C4-python-assignment.py
@@ -1,7 +1,8 @@
-# Imports to complete the exercises.
+# > Imports to complete the exercises.
 import math
 
-# EXERCISE 1: Create a list, tuple, float, integer, decimal, and dictionary.
+
+# > EXERCISE 1: Create a list, tuple, float, integer, decimal, and dictionary.
 towns = ["Barakaldo", "Sestao", "Portugalete", "Santurtzi"]
 brands = ("Samsung", "Xiaomi", "Apple", "BQ")
 age = 46
@@ -13,37 +14,37 @@ persona = {
     }
 
 
-
-# EXERCISE 2: Round your float up.
+# > EXERCISE 2: Round your float up.
 new_price = math.ceil(price)
 print(new_price)
 
 
-# EXERCISE 3: Get the square root of your float.
+# > EXERCISE 3: Get the square root of your float.
 square = math.sqrt(price)
 print(square)
 
 
-# EXERCISE 4: Select the first element from your dictionary.
+# > EXERCISE 4: Select the first element from your dictionary.
 first_persona = list(persona.keys())[0]
 print(first_persona)
 
 
-# EXERCISE 5: Select the second element from your tuple.
+# > EXERCISE 5: Select the second element from your tuple.
 new_brands = brands[1]
 print(new_brands)
 
 
-# EXERCISE 6: Add an element to the end of your list.
+# > EXERCISE 6: Add an element to the end of your list.
 towns.append('Sopuerta')
 print(towns)
 
 
-# EXERCISE 7: Replace the first element in your list.
+# > EXERCISE 7: Replace the first element in your list.
 towns[0] = "Amoroto"
 print(towns)
 
-# EXERCISE 8: Sort your list alphabetically.
+
+# > EXERCISE 8: Sort your list alphabetically.
 new_list = sorted(towns)
 print(new_list)
 
