240e6a78088b9fa3e2d1fa4a0e2b0db26a46f51e
Carlos Fernandez Miguel <carlosfernandezmiguel@gmail.com>
2026-03-05 20:12:07 +0000

    Last commit. Ready for correction!

--- M2C4-python-assignment.py
+++ M2C4-python-assignment.py
@@ -1,54 +1,48 @@
-# > Imports to complete the exercises.
+# > Imports to complete the exercises below:
+from decimal import Decimal
 import math
 
 
-# > EXERCISE 1: Create a list, tuple, float, integer, decimal, and dictionary.
-towns = ["Barakaldo", "Sestao", "Portugalete", "Santurtzi"]
-brands = ("Samsung", "Xiaomi", "Apple", "BQ")
-age = 46
-price = 18.20
+# > EXERCISE 1: Create a list, tuple, float, integer, decimal, and dictionary:
+towns = ["Barakaldo", "Sestao", "Portugalete", "Santurtzi"] # list
+brands = ("Samsung", "Xiaomi", "Apple", "BQ") # tuple
+age = 46 # integer
+price = 18.20 # float
+high = Decimal(1.86) # decimal
 persona = {
     "name" : "Manuel",
     "surname" : "Etxebarria",
     "city" : "Zarautz",
-    }
+    } # dictionary
 
 
-# > EXERCISE 2: Round your float up.
+# > EXERCISE 2: Round your float up:
 new_price = math.ceil(price)
-print(new_price)
 
 
-# > EXERCISE 3: Get the square root of your float.
+# > EXERCISE 3: Get the square root of your float:
 square = math.sqrt(price)
-print(square)
 
 
-# > EXERCISE 4: Select the first element from your dictionary.
-first_persona = list(persona.keys())[0]
-print(first_persona)
+# > EXERCISE 4: Select the first element from your dictionary:
+first_persona = list(persona.items())[0]
 
 
-# > EXERCISE 5: Select the second element from your tuple.
+# > EXERCISE 5: Select the second element from your tuple:
 new_brands = brands[1]
-print(new_brands)
 
 
-# > EXERCISE 6: Add an element to the end of your list.
+# > EXERCISE 6: Add an element to the end of your list:
 towns.append('Sopuerta')
-print(towns)
 
 
-# > EXERCISE 7: Replace the first element in your list.
+# > EXERCISE 7: Replace the first element in your list:
 towns[0] = "Amoroto"
-print(towns)
 
 
-# > EXERCISE 8: Sort your list alphabetically.
+# > EXERCISE 8: Sort your list alphabetically:
 new_list = sorted(towns)
-print(new_list)
 
 
-# EXERCISE 9: Use reassignment to add an element to your tuple.
-brands += ("Realme", "Oppo")
-print(brands)
+# EXERCISE 9: Use reassignment to add an element to your tuple:
+brands += ("Oppo",)
